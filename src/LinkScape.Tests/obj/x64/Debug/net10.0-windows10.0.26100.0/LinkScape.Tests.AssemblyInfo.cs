cd52da41b5b632828c4622c3064306f6a26df08d
john doyle <fizzledbydizzle@gmail.com>
2026-07-17 04:18:08 -0400

    add /bin oand obj ignore

--- src/LinkScape.Tests/obj/x64/Debug/net10.0-windows10.0.26100.0/LinkScape.Tests.AssemblyInfo.cs
+++ src/LinkScape.Tests/obj/x64/Debug/net10.0-windows10.0.26100.0/LinkScape.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinkScape.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a496864b9546ce8d0829e685a275777052644a3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7490b1a5cc83649b08803f17a569708ef6ecf2d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinkScape.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinkScape.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
